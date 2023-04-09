dd0edcf41725e8633cc03f230cb061ea8601eb49
adyelma figueiredo <99850329+Adyelmafigueiredo@users.noreply.github.com>
2023-04-08 23:10:34 -0300

    class operador

---
+++ tipos-variaveis/src/Operador.java
@@ -0,0 +1,17 @@
+public class Operador {
+    public static void main(String[] args) {
+        // operador de atribuição é =
+        //exemplo:
+        String nome = "Adyelma";
+        int idade = 33;
+        double peso = 62.500;
+        char sexo = 'M';
+
+        // operadores aritméticos são: + (adição), - (subtração), * (multiplicação) e / (divisão)
+        
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 34;
+        int multiplicacao = 20 * 8;
+
+    }
+}
---
+++ tipos-variaveis/src/TipoVariavel.java
@@ -0,0 +1,17 @@
+public class TipoVariavel {
+    public static void main(String[] args) throws Exception {
+        byte idade = 12;
+        short ano = 2023;
+        int cep = 22112233; //se começa com zero, terá quer mudar o tipo para String
+        Long telefone = 81988673321L; 
+        double salario = 1875.57;
+
+
+        final float PI= 3.14F; // a palavra reservada final torna uma váriavel em uma constante.
+
+        // as variáveis mais utilizadas são int, double e a class String.
+        
+
+
+    }
+}
