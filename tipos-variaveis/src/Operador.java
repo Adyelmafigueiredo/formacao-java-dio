95e8aec23a85834ed0f76ca682f895cbe1fb7d2f
adyelma figueiredo <99850329+Adyelmafigueiredo@users.noreply.github.com>
2023-04-17 16:21:39 -0300

    continuação dos operadores

--- tipos-variaveis/src/Operador.java
+++ tipos-variaveis/src/Operador.java
@@ -1,3 +1,5 @@
+import javax.swing.plaf.synth.SynthPasswordFieldUI;
+
 public class Operador {
     public static void main(String[] args) {
         // operador de atribuição é =
@@ -12,6 +14,38 @@ public class Operador {
         double soma = 10.5 + 15.7;
         int subtracao = 113 - 34;
         int multiplicacao = 20 * 8;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3; // devolve o resto da divisão
+        double resultado = (10 *    7) + (20 / 4);
+        String curso = "formação " + "Java"; // concatenação
+        System.out.println(curso);
 
+        // operador Unário: incrementar (++), decrementar (--), inverter valores numéricos (+ ou -) e booleanos (!)
+        int numero = 5;
+         System.out.println(- numero);
+         System.out.println(numero);
+
+         numero = - numero; // deixar o numero negativo
+         System.out.println(numero);
+         numero = numero * -1;
+
+        // incremento
+        int variavel = 5;
+        
+        System.out.println(variavel ++); 
+        System.out.println(variavel);
+
+        // decremento
+        System.out.println(-- variavel);
+
+        //negação
+        boolean mentira = true;
+        System.out.println(!mentira);
+        System.out.println(mentira);
+        mentira = !mentira;
+        System.out.println(mentira);
+
+        //Operador Ternário
+        
     }
 }
